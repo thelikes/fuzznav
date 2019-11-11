9dbfd5da3f3b38934e40e898977031e3c1300ed6
fuzzerk <prezzure@protonmail.com>
2019-11-11 14:43:02 -0800

    targs mode pass 1 complete

--- main.go
+++ main.go
@@ -48,11 +48,27 @@ func main() {
 		}
 	}
 
-	fmt.Println(results)
+	/*
+		for _, a_res := range results {
+			resultPrettyPrint(a_res)
+		}
+	*/
 
 	switch mode {
 	case "targs":
-		fmt.Println("Printing targs")
+		ep_map := make(map[string][]string)
+
+		// iterate through the results
+		for _, a_res := range results {
+			// append the new wordlist to the existing list of wordlists
+			ep_map[a_res.Target] = targetBuildWordlistList(ep_map[a_res.Target], a_res.Wordlist)
+
+		}
+
+		for key, value := range ep_map {
+			fmt.Printf("%s:%s\n", key, targetPrettyPrintWordlists(value))
+		}
+
 	}
 	// identify - identify tool used to generate results
 
@@ -62,7 +78,48 @@ func main() {
 	//  or iterate though targets list and print endpoints
 }
 
-func getWordlistName(raw string) string {
+func targetPrettyPrintWordlists(list []string) string {
+	// nicely print the list of wordlists
+	ret := ""
+
+	for i := 0; i < len(list); i++ {
+		if i == len(list)-1 {
+			ret += list[i]
+		} else {
+			ret = ret + list[i] + ","
+		}
+	}
+
+	return ret
+}
+
+func targetBuildWordlistList(existing []string, addition string) []string {
+	// pretty append wordlist to list of wordlists
+	if !sliceContains(existing, addition) {
+		existing = append(existing, addition)
+	}
+
+	//fmt.Println("existing:", existing)
+	//fmt.Println("a_res.Wordlist:", addition)
+
+	return existing
+}
+
+func sliceContains(slice []string, needle string) bool {
+	/*
+	 * Check if a slice contains a value
+	 */
+
+	for _, str := range slice {
+		if str == needle {
+			return true
+		}
+	}
+
+	return false
+}
+
+func resultGetWordlistName(raw string) string {
 	// get the file name from the full path
 	filestr := path.Base(raw)
 	//fmt.Println("filestr: ", filestr)
@@ -84,6 +141,7 @@ func getWordlistName(raw string) string {
 		//fmt.Println("slice: ", s[i])
 		if get_wordlist == false {
 			wordlist += s[i] + "-"
+			// wordlist has to end in '.txt'
 			if strings.Contains(s[i], ".txt") {
 				//println("gotcha")
 				get_wordlist = true
@@ -97,6 +155,34 @@ func getWordlistName(raw string) string {
 	return wordlist
 }
 
+func resultGetTarget(filename string) string {
+	// extract the target
+	slice := strings.Split(filename, "-")
+
+	return slice[len(slice)-1]
+
+}
+
+func resultGetTool(filename string) string {
+	// this one is easy because the tool is the first index of delimeter '-'
+	// gobuster-directory-list-2.3-small.txt-http___towers.att.com.txt
+	str := strings.Split(filename, "-")
+	var tool string
+
+	switch str[0] {
+	case "ffuf":
+		tool = "ffuf"
+	case "gobuster":
+		tool = "gobuster"
+	default:
+		tool = "unkown"
+	}
+
+	// extract the target
+	return tool
+
+}
+
 // parse the filename to extract the target and the wordlist
 func processFile(filepath string) bool {
 	var res Result
@@ -105,12 +191,18 @@ func processFile(filepath string) bool {
 
 	// ensure we're only going after output files
 	if path.Ext(res.Filename) == ".txt" {
-		fmt.Println("Processing file", res.Filename)
+		//fmt.Println("Processing file", res.Filename)
 
 		// parse out the wordlist
-		res.Wordlist = getWordlistName(res.Filename)
+		res.Wordlist = resultGetWordlistName(res.Filename)
 
-		fmt.Println("  Wordlist:", res.Wordlist)
+		// parse out the tool
+		res.Tool = resultGetTool(res.Filename)
+
+		// parse out the target
+		res.Target = resultGetTarget(res.Filename)
+
+		//fmt.Println("  Wordlist:", res.Wordlist)
 
 		results = append(results, res)
 
@@ -125,4 +217,11 @@ func fileExists(filepath string) bool {
 		return false
 	}
 	return !info.IsDir()
+}
+
+func resultPrettyPrint(res Result) {
+	fmt.Println("Filename:", res.Filename)
+	fmt.Println("Wordlist:", res.Wordlist)
+	fmt.Println("Target:", res.Target)
+	fmt.Println("Tool:", res.Tool)
 }

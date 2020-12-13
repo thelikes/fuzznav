733ba369c2f1a053901f28559e5ecfad3f3efb0f
fuzzerk <prezzure@protonmail.com>
2020-12-13 13:15:29 -0800

    able to read and ingest json, parse and print portions of results and config

--- main.go
+++ main.go
@@ -8,20 +8,62 @@ import (
 	"os"
 )
 
-// ffuf json struct
-type FfufCommandLine struct {
-	Commandline string
+// global debug mode used with env variable
+var debugmode bool
+
+type FfufCmdLine struct {
+	CommandLine string `json:"commandline"`
+}
+
+type FfufTime struct {
+	Time string `json:"time"`
+}
+
+//
+type FfufConfig struct {
+	Config FfufMetaData `json:"config"`
+}
+
+// ffuf json 'commandline' struct
+type FfufMetaData struct {
+	URL    string `json:"url"`
+	Method string `json:"method"`
+	//Time    string `json:"time"`
+}
+
+// Array of ffuf {"results":..}
+type FfufResults struct {
+	Results []FfufResult `json:"results"`
+}
+
+// ffuf json 'results' struct - status, size, words, lines
+type FfufResult struct {
+	Input    string `json:"input"`
+	Position int    `json:"position"`
+	Status   int    `json:"status"`
+	Length   int    `json:"length"`
+	Words    int    `json:"words"`
+	Lines    int    `json:"lines"`
+	Host     string `json:"host"`
+	URL      string `json:"url"`
 }
 
 func main() {
+	// proces debug mode
+	setDebugMode()
+
 	// read filenames from stdin
 	sc := bufio.NewScanner(os.Stdin)
 	for sc.Scan() {
-		fmt.Printf("[debug] filename: %v\n", sc.Text())
+		if debugmode {
+			fmt.Printf("[debug] filename: %v\n", sc.Text())
+		}
 		fn_str := sc.Text()
 
+		// read & process file
 		if fileExists(fn_str) {
-			processFile(fn_str)
+			byteVal := processFile(fn_str)
+			parseResults(byteVal)
 		}
 	}
 }
@@ -36,8 +78,8 @@ func fileExists(filepath string) bool {
 	return !info.IsDir()
 }
 
-// parse the filename to extract the target, tool, and the wordlist
-func processFile(filepath string) {
+// ingest json to byte array
+func processFile(filepath string) []byte {
 	// read file
 	jsonFile, err := os.Open(filepath)
 
@@ -46,9 +88,46 @@ func processFile(filepath string) {
 	}
 	byteVal, _ := ioutil.ReadAll(jsonFile)
 
-	var cmdline FfufCommandLine
-	json.Unmarshal(byteVal, &cmdline)
-	fmt.Printf("[debug] command line: %v\n", cmdline)
-
 	defer jsonFile.Close()
+
+	return byteVal
+}
+
+// collect values
+func parseResults(byteVal []byte) {
+	var commandline FfufCmdLine
+	json.Unmarshal(byteVal, &commandline)
+
+	var time FfufTime
+	json.Unmarshal(byteVal, &time)
+
+	var metadata FfufConfig
+	json.Unmarshal(byteVal, &metadata)
+
+	var results FfufResults
+	json.Unmarshal(byteVal, &results)
+
+	if debugmode {
+		fmt.Printf("[debug] command line: %v\n", commandline.CommandLine)
+		fmt.Printf("[debug] target: %v\n", metadata.Config.URL)
+		fmt.Printf("[debug] method: %v\n", metadata.Config.Method)
+		fmt.Printf("[debug] time: %v\n", time.Time)
+		fmt.Printf("[debug] url: %v\n", results.Results[0].URL)
+	}
+
+	fmt.Printf("%v %v %v %v %v\n", results.Results[0].URL, results.Results[0].Status, results.Results[0].Length, results.Results[0].Words, results.Results[0].Lines)
+
+}
+
+// set debug mode
+func setDebugMode() {
+	debug_str := os.Getenv("DEBUG")
+
+	fmt.Printf("mode=%v\n", debug_str)
+	if debug_str == "true" {
+		fmt.Println("[*] Debug mode set to true.")
+		debugmode = true
+	} else {
+		debugmode = false
+	}
 }

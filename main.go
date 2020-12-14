c9185b66ed32f03ce265571f2868673506a98290
fuzzerk <prezzure@protonmail.com>
2020-12-13 20:50:07 -0800

    working endpoint printing

--- main.go
+++ main.go
@@ -4,8 +4,10 @@ import (
 	"bufio"
 	"encoding/json"
 	"fmt"
+	"github.com/fatih/color"
 	"io/ioutil"
 	"os"
+	"text/tabwriter"
 )
 
 // global debug mode used with env variable
@@ -56,7 +58,10 @@ func main() {
 	// proces debug mode
 	setDebugMode()
 
-	// read filenames from stdin
+	// slice to store slices of NavEndpoints
+	var results [][]NavEndpoints
+
+	// read and parse each filename from stdin
 	sc := bufio.NewScanner(os.Stdin)
 	for sc.Scan() {
 		if debugmode {
@@ -64,12 +69,15 @@ func main() {
 		}
 		fn_str := sc.Text()
 
-		// read & process file
+		// read & process results from one file
 		if fileExists(fn_str) {
 			byteVal := processFile(fn_str)
-			parseResults(byteVal)
+			results = append(results, parseResults(byteVal))
+			//fmt.Printf("len(results) = %v\n", len(results))
 		}
 	}
+
+	endpointsMap(results)
 }
 
 // check if file exists and is not directory
@@ -98,8 +106,9 @@ func processFile(filepath string) []byte {
 }
 
 // collect values
-func parseResults(byteVal []byte) NavEndpoints {
+func parseResults(byteVal []byte) []NavEndpoints {
 	var endpoints NavEndpoints
+	var endpointResults []NavEndpoints
 
 	var output FfufOutput
 	json.Unmarshal(byteVal, &output)
@@ -114,17 +123,85 @@ func parseResults(byteVal []byte) NavEndpoints {
 		fmt.Printf("[debug] url: %v\n", output.Results[0].URL)
 	}
 
-	endpoints.Endpoint = output.Results[0].URL
-	endpoints.Status = output.Results[0].Status
-	endpoints.Length = output.Results[0].Length
-	endpoints.Words = output.Results[0].Words
-	endpoints.Lines = output.Results[0].Lines
+	//fmt.Printf("len: %v\n", len(output.Results))
+	for i := 0; i < len(output.Results); i++ {
+		endpoints.Endpoint = output.Results[i].URL
+		endpoints.Status = output.Results[i].Status
+		endpoints.Length = output.Results[i].Length
+		endpoints.Words = output.Results[i].Words
+		endpoints.Lines = output.Results[i].Lines
 
-	if debugmode {
-		fmt.Printf("%v [Status: %v, Size: %v, Words: %v, Lines: %v]\n", endpoints.Endpoint, endpoints.Status, endpoints.Length, endpoints.Words, endpoints.Lines)
+		if debugmode {
+			fmt.Printf("%v [Status: %v, Size: %v, Words: %v, Lines: %v]\n", endpoints.Endpoint, endpoints.Status, endpoints.Length, endpoints.Words, endpoints.Lines)
+		}
+
+		endpointResults = append(endpointResults, endpoints)
+
 	}
 
-	return endpoints
+	return endpointResults
+}
+
+// print map of endpoints
+func endpointsMap(results [][]NavEndpoints) {
+	endpoints := processEndpoints(results)
+	red := color.New(color.FgRed).SprintFunc()
+
+	w := new(tabwriter.Writer)
+	w.Init(os.Stdout, 0, 8, 2, '\t', tabwriter.AlignRight)
+	for _, ep := range endpoints {
+		str := fmt.Sprintf("%v\t[Status: %v, Size: %v, Words: %v, Lines: %v]\t", ep.Endpoint, red(ep.Status), ep.Length, ep.Words, ep.Lines)
+
+		fmt.Fprintln(w, str)
+	}
+
+	w.Flush()
+}
+
+// return a unique list of all endpoints
+func processEndpoints(results [][]NavEndpoints) []NavEndpoints {
+	var allEndpoints []string
+	var cleanResults []NavEndpoints
+
+	// for each slice in results
+	for _, res := range results {
+		// for each result in results
+		for _, ep := range res {
+			// only store an endpoint if not already stored
+			if !sliceContains(allEndpoints, ep.Endpoint) {
+				allEndpoints = append(allEndpoints, ep.Endpoint)
+				cleanResults = append(cleanResults, ep)
+			}
+		}
+	}
+
+	return cleanResults
+}
+
+// check if a slice contains an entry
+func sliceContains(slice []string, needle string) bool {
+	for _, str := range slice {
+		if str == needle {
+			return true
+		}
+	}
+
+	return false
+}
+
+// nicely print the list of wordlists
+func targetPrettyPrintWordlists(list []string) string {
+	ret := ""
+
+	for i := 0; i < len(list); i++ {
+		if i == len(list)-1 {
+			ret += list[i]
+		} else {
+			ret = ret + list[i] + ","
+		}
+	}
+
+	return ret
 }
 
 // set debug mode
